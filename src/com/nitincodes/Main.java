ce2587fbc3f469368494265ac8fa495631fff972
Nitin <nsinghboss02@gmail.com>
2021-09-03 14:41:34 +0530

    Initial commit

---
+++ src/com/nitincodes/Main.java
@@ -0,0 +1,27 @@
+package com.nitincodes;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// java program to print reverse diagonal of an array
+        Scanner sc = new Scanner(System.in);
+        int a = 0;
+        int arr[][] = new int[3][3];
+    for(int i=0;i<3;i++){
+        for(int j=0;j<3;j++){
+            arr[i][j] = sc.nextInt();
+        }
+    }
+    for(int i=0;i<3;i++){
+        for(int j=0;j<=3-i-1;j++){
+            System.out.println(arr[i][j]);
+        }
+    }
+
+
+
+
+
+    }
+}
